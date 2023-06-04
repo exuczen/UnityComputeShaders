5997e23b3bc438d6b4bb2271c378e63d4e4e0fba
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-06-04 22:32:51 +0200

    Section 3: Post Processing: 19-20. A star glow effect

--- UnityComputeShaders - start/Assets/Scripts/StarGlow.cs
+++ UnityComputeShaders - start/Assets/Scripts/StarGlow.cs
@@ -1,9 +1,30 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using static StarGlow;
 
 public class StarGlow : MonoBehaviour
 {
+    //public enum CompositeType
+    //{
+    //    _COMPOSITE_TYPE_ADDITIVE = 0,
+    //    _COMPOSITE_TYPE_SCREEN = 1,
+    //    _COMPOSITE_TYPE_COLORED_ADDITIVE = 2,
+    //    _COMPOSITE_TYPE_COLORED_SCREEN = 3,
+    //    _COMPOSITE_TYPE_DEBUG = 4
+    //}
+
     #region Field
+
+    //private static Dictionary<CompositeType, string> CompositeTypes = new Dictionary<CompositeType, string>()
+    //{
+    //    { CompositeType._COMPOSITE_TYPE_ADDITIVE,         CompositeType._COMPOSITE_TYPE_ADDITIVE.ToString()         },
+    //    { CompositeType._COMPOSITE_TYPE_SCREEN,           CompositeType._COMPOSITE_TYPE_SCREEN.ToString()           },
+    //    { CompositeType._COMPOSITE_TYPE_COLORED_ADDITIVE, CompositeType._COMPOSITE_TYPE_COLORED_ADDITIVE.ToString() },
+    //    { CompositeType._COMPOSITE_TYPE_COLORED_SCREEN,   CompositeType._COMPOSITE_TYPE_COLORED_SCREEN.ToString()   },
+    //    { CompositeType._COMPOSITE_TYPE_DEBUG,            CompositeType._COMPOSITE_TYPE_DEBUG.ToString()            }
+    //};
+
+    //public StarGlow.CompositeType compositeType = StarGlow.CompositeType._COMPOSITE_TYPE_ADDITIVE;
 
     [Range(0, 1)]
     public float threshold = 1;
@@ -30,28 +51,66 @@ public class StarGlow : MonoBehaviour
 
     public Color color = Color.white;
 
-    private int compositeTexID   = 0;
+    private int compositeTexID = 0;
     private int compositeColorID = 0;
-    private int brightnessSettingsID   = 0;
-    private int iterationID      = 0;
-    private int offsetID         = 0;
+    private int brightnessSettingsID = 0;
+    private int iterationID = 0;
+    private int offsetID = 0;
 
     #endregion Field
 
     #region Method
 
-    void Start()
+    private void Start()
     {
-        compositeTexID   = Shader.PropertyToID("_CompositeTex");
+        compositeTexID = Shader.PropertyToID("_CompositeTex");
         compositeColorID = Shader.PropertyToID("_CompositeColor");
-        brightnessSettingsID   = Shader.PropertyToID("_BrightnessSettings");
-        iterationID      = Shader.PropertyToID("_Iteration");
-        offsetID         = Shader.PropertyToID("_Offset");
+        brightnessSettingsID = Shader.PropertyToID("_BrightnessSettings");
+        iterationID = Shader.PropertyToID("_Iteration");
+        offsetID = Shader.PropertyToID("_Offset");
     }
 
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Graphics.Blit(source, destination);
+        var brightnessTex = RenderTexture.GetTemporary(source.width / divide, source.height / divide, source.depth, source.format);
+        var blurredTex1 = RenderTexture.GetTemporary(brightnessTex.descriptor);
+        var blurredTex2 = RenderTexture.GetTemporary(brightnessTex.descriptor);
+        var compositeTex = RenderTexture.GetTemporary(brightnessTex.descriptor);
+
+        material.SetVector(brightnessSettingsID, new Vector3(threshold, intensity, attenuation));
+        Graphics.Blit(source, brightnessTex, material, 1);
+
+        float angle = 360f / numOfStreaks;
+        for (int x = 1; x <= numOfStreaks; x++)
+        {
+            var offset = (Quaternion.AngleAxis(angle * x + angleOfStreak, Vector3.forward) * Vector2.down).normalized;
+            material.SetVector(offsetID, offset);
+            material.SetInt(iterationID, 1);
+
+            Graphics.Blit(brightnessTex, blurredTex1, material, 2);
+
+            for (int i = 2; i <= iteration; i++)
+            {
+                material.SetInt(iterationID, i);
+                Graphics.Blit(blurredTex1, blurredTex2, material, 2);
+
+                (blurredTex2, blurredTex1) = (blurredTex1, blurredTex2);
+            }
+            Graphics.Blit(blurredTex1, compositeTex, material, 3);
+        }
+
+        //material.EnableKeyword(StarGlow.CompositeTypes[compositeType]);
+        material.SetColor(compositeColorID, color);
+        material.SetTexture(compositeTexID, compositeTex);
+
+        Graphics.Blit(source, destination, material, 4);
+
+        //material.DisableKeyword(StarGlow.CompositeTypes[compositeType]);
+
+        RenderTexture.ReleaseTemporary(brightnessTex);
+        RenderTexture.ReleaseTemporary(blurredTex1);
+        RenderTexture.ReleaseTemporary(blurredTex2);
+        RenderTexture.ReleaseTemporary(compositeTex);
     }
 
     #endregion Method
--- UnityComputeShaders - start/Assets/Shaders/StarGlow.shader
+++ UnityComputeShaders - start/Assets/Shaders/StarGlow.shader
@@ -4,6 +4,10 @@
     {
         [HideInInspector]
         _MainTex("Texture", 2D) = "white" {}
+
+        //[KeywordEnum(ADDITIVE, SCREEN, COLORED_ADDITIVE, COLORED_SCREEN, DEBUG)]
+        //_COMPOSITE_TYPE("Composite Type", Float) = 0
+
         _BrightnessSettings("(Threshold, Intensity, Attenuation, -)", Vector) = (0.8, 1.0, 0.95, 0.0)
     }
     SubShader
@@ -23,10 +27,7 @@
 
         ENDCG
 
-        // STEP:0
-        // Debug.
-
-        Pass
+        Pass //Step 0
         {
             CGPROGRAM
 
@@ -36,6 +37,124 @@
             fixed4 frag(v2f_img input) : SV_Target
             {
                 return tex2D(_MainTex, input.uv);
+            }
+
+            ENDCG
+        }
+
+        Pass //Step 1
+        {
+            CGPROGRAM
+
+            #pragma vertex vert_img
+            #pragma  fragment frag
+            
+            fixed4 frag(v2f_img input) : SV_Target
+            {
+                float4 color = tex2D(_MainTex, input.uv);
+                return max(color - BRIGHTNESS_THRESHOLD, 0) * INTENSITY;
+            }
+
+            ENDCG
+        }
+
+        Pass //Step 2
+        {
+            CGPROGRAM
+
+            #pragma vertex vert
+            #pragma  fragment frag
+            
+            int _Iteration;
+            float2 _Offset;
+
+            struct v2f_starglow
+            {
+                float4 pos      : SV_POSITION;
+                half2 uv        : TEXCOORD0;
+                half power      : TEXCOORD1;
+                half2 offset    : TEXCOORD2;
+            };
+
+            v2f_starglow vert(appdata_img v)
+            {
+                v2f_starglow o;
+
+                o.pos       = UnityObjectToClipPos(v.vertex);
+                o.uv        = v.texcoord;
+                o.power     = pow(4, _Iteration - 1);
+                o.offset    = _MainTex_TexelSize.xy * _Offset * o.power;
+
+                return o;
+            }
+
+            float4 frag(v2f_starglow input) : SV_TARGET
+            {
+                half4 color = 0;
+                half2 uv = input.uv;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    color += saturate(tex2D(_MainTex, uv) * pow(ATTENUATION, input.power * j));
+                    uv += input.offset;
+                }
+                return color;
+            }
+            ENDCG
+        }
+
+        Pass //Step 3
+        {
+            Blend OneMinusDstColor One
+            
+            CGPROGRAM
+
+            #pragma vertex vert_img
+            #pragma  fragment frag
+            
+            fixed4 frag(v2f_img input) : SV_Target
+            {
+                return tex2D(_MainTex, input.uv);
+            }
+
+            ENDCG
+        }
+
+        Pass //Step 4
+        {
+            CGPROGRAM
+
+            #pragma vertex vert_img
+            #pragma  fragment frag
+            //#pragma multi_compile _COMPOSITE_TYPE_ADDITIVE _COMPOSITE_TYPE_SCREEN _COMPOSITE_TYPE_COLORED_ADDITIVE _COMPOSITE_TYPE_COLORED_SCREEN _COMPOSITE_TYPE_DEBUG
+            
+            sampler2D _CompositeTex;
+            float4 _CompositeColor;
+
+            fixed4 frag(v2f_img input) : SV_Target
+            {
+                float4 mainColor      = tex2D(_MainTex,      input.uv);
+                float4 compositeColor = tex2D(_CompositeTex, input.uv);
+
+                //#if defined(_COMPOSITE_TYPE_COLORED_ADDITIVE) || defined(_COMPOSITE_TYPE_COLORED_SCREEN)
+
+                compositeColor.rgb = (compositeColor.r + compositeColor.g + compositeColor.b) * 0.3333 * _CompositeColor;
+
+                //#endif
+
+                //#if defined(_COMPOSITE_TYPE_SCREEN) || defined(_COMPOSITE_TYPE_COLORED_SCREEN)
+
+                //return saturate(mainColor + compositeColor - saturate(mainColor * compositeColor));
+
+                //#elif defined(_COMPOSITE_TYPE_ADDITIVE) || defined(_COMPOSITE_TYPE_COLORED_ADDITIVE)
+
+                return saturate(mainColor + compositeColor);
+
+                //#else
+
+                //return compositeColor;
+
+                //#endif
             }
 
             ENDCG
