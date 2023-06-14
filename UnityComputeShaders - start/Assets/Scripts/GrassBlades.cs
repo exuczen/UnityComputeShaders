c6225b7600c3f17b0efefed21caa37b720b01677
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-06-14 21:13:59 +0200

    Section 6: Grass: 37. Bending blades of grass

--- UnityComputeShaders - start/Assets/Scripts/GrassBlades.cs
+++ UnityComputeShaders - start/Assets/Scripts/GrassBlades.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class GrassBlades : MonoBehaviour
 {
-    struct GrassBlade
+    private static readonly int SIZE_GRASS_BLADE = Marshal.SizeOf<GrassBlade>(); //6 * sizeof(float);
+
+    private struct GrassBlade
     {
         public Vector3 position;
         public float bend;
         public float noise;
         public float fade;
 
-        public GrassBlade( Vector3 pos)
+        public GrassBlade(Vector3 pos)
         {
             position.x = pos.x;
             position.y = pos.y;
@@ -21,15 +24,14 @@ public class GrassBlades : MonoBehaviour
             fade = Random.Range(0.5f, 1);
         }
     }
-    int SIZE_GRASS_BLADE = 6 * sizeof(float);
 
     public Material material;
     public ComputeShader shader;
     public Material visualizeNoise;
     public bool viewNoise = false;
-    [Range(0,1)]
+    [Range(0, 1)]
     public float density;
-    [Range(0.1f,3)]
+    [Range(0.1f, 3)]
     public float scale;
     [Range(10, 45)]
     public float maxBend;
@@ -40,16 +42,16 @@ public class GrassBlades : MonoBehaviour
     [Range(10, 1000)]
     public float windScale;
 
-    ComputeBuffer bladesBuffer;
-    ComputeBuffer argsBuffer;
-    GrassBlade[] bladesArray;
-    uint[] argsArray = new uint[] { 0, 0, 0, 0, 0 };
-    Bounds bounds;
-    int timeID;
-    int groupSize;
-    int kernelBendGrass;
-    Mesh blade;
-    Material groundMaterial;
+    private ComputeBuffer bladesBuffer;
+    private ComputeBuffer argsBuffer;
+    private GrassBlade[] bladesArray;
+    private readonly uint[] argsArray = new uint[] { 0, 0, 0, 0, 0 };
+    private Bounds bounds;
+    private int timeID;
+    private int groupSize;
+    private int kernelBendGrass;
+    private Mesh blade;
+    private Material groundMaterial;
 
     Mesh Blade
     {
@@ -66,29 +68,62 @@ public class GrassBlades : MonoBehaviour
                 mesh = new Mesh();
 
                 float height = 0.2f;
-                float rowHeight = height / 4;
-                float halfWidth = height / 10;
+                float rowHeight = height / 4f;
+                float halfWidth = height / 10f;
 
                 //1. Use the above variables to define the vertices array
+                var vertices = new Vector3[]
+                {
+                    new Vector3(-halfWidth, 0f, 0f),
+                    new Vector3( halfWidth, 0f, 0f),
+                    new Vector3(-halfWidth, rowHeight, 0f),
+                    new Vector3( halfWidth, rowHeight, 0f),
+                    new Vector3(-halfWidth * 0.9f, rowHeight * 2f, 0f),
+                    new Vector3( halfWidth * 0.9f, rowHeight * 2f, 0f),
+                    new Vector3(-halfWidth * 0.8f, rowHeight * 3f, 0f),
+                    new Vector3( halfWidth * 0.8f, rowHeight * 3f, 0f),
+                    new Vector3( 0f, rowHeight * 4f, 0f),
+                };
 
                 //2. Define the normals array, hint: each vertex uses the same normal
-                Vector3 normal = new Vector3(0, 0, -1);
+                var normal = new Vector3(0, 0, -1);
+                var normals = new Vector3[vertices.Length];
+                for (int i = 0; i < vertices.Length; i++)
+                {
+                    normals[i] = normal;
+                }
 
                 //3. Define the uvs array
+                var uvs = new Vector2[vertices.Length];
+                for (int i = 0; i < vertices.Length; i++)
+                {
+                    uvs[i].x = 0.5f + 0.5f * vertices[i].x / halfWidth; //(vertices[i].x + halfWidth) / (2f * halfWidth)
+                    uvs[i].y = vertices[i].y / height;
+                }
 
                 //4. Define the indices array
+                var indices = new int[]
+                {
+                    0, 1, 2, 1, 3, 2,
+                    2, 3, 4, 3, 5, 4,
+                    4, 5, 6, 5, 7, 6,
+                    6, 7, 8
+                };
 
                 //5. Assign the mesh properties using the arrays
                 //   for indices use
-                //   mesh.SetIndices( indices, MeshTopology.Triangles, 0 );
-
+                //   mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+                mesh.vertices = vertices;
+                mesh.normals = normals;
+                mesh.uv = uvs;
+                mesh.SetIndices(indices, MeshTopology.Triangles, 0);
             }
 
             return mesh;
         }
     }
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         bounds = new Bounds(Vector3.zero, new Vector3(30, 30, 30));
         blade = Blade;
@@ -107,14 +142,20 @@ public class GrassBlades : MonoBehaviour
 
             renderer.material = (viewNoise) ? visualizeNoise : groundMaterial;
 
-            //TO DO: set wind using wind direction, speed and noise scale
-            Vector4 wind = new Vector4();
+            //TODO: set wind using wind direction, speed and noise scale
+            var wind = new Vector4
+            {
+                x = Mathf.Cos(windDirection * Mathf.Deg2Rad),
+                y = Mathf.Sin(windDirection * Mathf.Deg2Rad),
+                z = windSpeed,
+                w = windScale
+            };
             shader.SetVector("wind", wind);
             visualizeNoise.SetVector("wind", wind);
         }
     }
 
-    void InitShader()
+    private void InitShader()
     {
         MeshFilter mf = GetComponent<MeshFilter>();
         Bounds bounds = mf.sharedMesh.bounds;
@@ -128,18 +169,20 @@ public class GrassBlades : MonoBehaviour
 
         kernelBendGrass = shader.FindKernel("BendGrass");
 
-        uint threadGroupSize;
-        shader.GetKernelThreadGroupSizes(kernelBendGrass, out threadGroupSize, out _, out _);
-        groupSize = Mathf.CeilToInt((float)total / (float)threadGroupSize);
+        shader.GetKernelThreadGroupSizes(kernelBendGrass, out uint threadGroupSize, out _, out _);
+        groupSize = Mathf.CeilToInt((float)total / threadGroupSize);
         int count = groupSize * (int)threadGroupSize;
 
         bladesArray = new GrassBlade[count];
 
-        for(int i=0; i<count; i++)
+        for (int i = 0; i < count; i++)
         {
-            Vector3 pos = new Vector3( Random.value * bounds.extents.x * 2 - bounds.extents.x + bounds.center.x,
-                                       0,
-                                       Random.value * bounds.extents.z * 2 - bounds.extents.z + bounds.center.z);
+            var pos = new Vector3
+            {
+                x = Random.Range(-1f, 1f) * bounds.extents.x + bounds.center.x,
+                y = 0,
+                z = Random.Range(-1f, 1f) * bounds.extents.z + bounds.center.z
+            };
             pos = transform.TransformPoint(pos);
             bladesArray[i] = new GrassBlade(pos);
         }
@@ -149,8 +192,14 @@ public class GrassBlades : MonoBehaviour
 
         shader.SetBuffer(kernelBendGrass, "bladesBuffer", bladesBuffer);
         shader.SetFloat("maxBend", maxBend * Mathf.PI / 180);
-        //TO DO: set wind using wind direction, speed and noise scale
-        Vector4 wind = new Vector4();
+        //TODO: set wind using wind direction, speed and noise scale
+        var wind = new Vector4
+        {
+            x = Mathf.Cos(windDirection * Mathf.Deg2Rad),
+            y = Mathf.Sin(windDirection * Mathf.Deg2Rad),
+            z = windSpeed,
+            w = windScale
+        };
         shader.SetVector("wind", wind);
 
         timeID = Shader.PropertyToID("time");
@@ -164,8 +213,7 @@ public class GrassBlades : MonoBehaviour
         material.SetFloat("_Scale", scale);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         shader.SetFloat(timeID, Time.time);
         shader.Dispatch(kernelBendGrass, groupSize, 1, 1);
