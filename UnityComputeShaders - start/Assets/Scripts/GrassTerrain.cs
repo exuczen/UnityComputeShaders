4ad410417df844dddd6162a2489bcea20cc13d85
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-06-15 23:16:06 +0200

    Section 6: Grass: 38. Grass on an uneven surface

--- UnityComputeShaders - start/Assets/Scripts/GrassTerrain.cs
+++ UnityComputeShaders - start/Assets/Scripts/GrassTerrain.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GrassTerrain : MonoBehaviour
 {
-    struct GrassClump
+    private static readonly int SIZE_GRASS_CLUMP = 5 * sizeof(float);
+
+    private struct GrassClump
     {
         public Vector3 position;
         public float lean;
         public float noise;
 
-        public GrassClump( Vector3 pos)
+        public GrassClump(Vector3 pos)
         {
             position.x = pos.x;
             position.y = pos.y;
@@ -20,37 +22,36 @@ public class GrassTerrain : MonoBehaviour
             if (Random.value < 0.5f) noise = -noise;
         }
     }
-    int SIZE_GRASS_CLUMP = 5 * sizeof(float);
 
     public Mesh mesh;
     public Material material;
     public ComputeShader shader;
-    [Range(0,3)]
+    [Range(0, 3)]
     public float density = 0.8f;
-    [Range(0.1f,3)]
+    [Range(0.1f, 3)]
     public float scale = 0.2f;
     [Range(10, 45)]
     public float maxLean = 25;
     [Range(0, 1)]
     public float heightAffect = 0.5f;
 
-    ComputeBuffer clumpsBuffer;
-    ComputeBuffer argsBuffer;
-    GrassClump[] clumpsArray;
-    uint[] argsArray = new uint[] { 0, 0, 0, 0, 0 };
-    Bounds bounds;
-    int timeID;
-    int groupSize;
-    int kernelLeanGrass;
+    private ComputeBuffer clumpsBuffer;
+    private ComputeBuffer argsBuffer;
+    private GrassClump[] clumpsArray;
+    private readonly uint[] argsArray = new uint[] { 0, 0, 0, 0, 0 };
+    private Bounds bounds;
+    private int timeID;
+    private int groupSize;
+    private int kernelLeanGrass;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         bounds = new Bounds(Vector3.zero, new Vector3(30, 30, 30));
         InitShader();
     }
 
-    void InitShader()
+    private void InitShader()
     {
         MeshFilter mf = GetComponent<MeshFilter>();
         Bounds bounds = mf.sharedMesh.bounds;
@@ -64,9 +65,8 @@ public class GrassTerrain : MonoBehaviour
 
         kernelLeanGrass = shader.FindKernel("LeanGrass");
 
-        uint threadGroupSize;
-        shader.GetKernelThreadGroupSizes(kernelLeanGrass, out threadGroupSize, out _, out _);
-        groupSize = Mathf.CeilToInt((float)total / (float)threadGroupSize);
+        shader.GetKernelThreadGroupSizes(kernelLeanGrass, out uint threadGroupSize, out _, out _);
+        groupSize = Mathf.CeilToInt((float)total / threadGroupSize);
         int count = groupSize * (int)threadGroupSize;
 
         InitPositionsArray(count, bounds);
@@ -89,13 +89,53 @@ public class GrassTerrain : MonoBehaviour
         material.SetFloat("_Scale", scale);
     }
 
-    void InitPositionsArray(int count, Bounds bounds)
+    private void InitPositionsArray(int count, Bounds bounds)
     {
         clumpsArray = new GrassClump[count];
+
+        gameObject.AddComponent<MeshCollider>();
+
+        var v = new Vector3(0f, bounds.max.y, 0f);
+        v = transform.TransformPoint(v);
+        float maxY = v.y;
+
+        v.Set(0f, bounds.min.y, 0f);
+        v = transform.TransformPoint(v);
+        float minY = v.y;
+
+        float range = bounds.size.y; //maxY - minY; 
+        float castY = maxY + 10f;
+
+        int loopCount = 0;
+        int index = 0;
+
+        while (index < count && loopCount < 10 * count)
+        {
+            loopCount++;
+            var pos = new Vector3
+            {
+                x = Random.Range(bounds.min.x, bounds.max.x),
+                y = 0,
+                z = Random.Range(bounds.min.z, bounds.max.z),
+            };
+            pos = transform.TransformPoint(pos);
+            pos.y = castY;
+
+            if (Physics.Raycast(pos, Vector3.down, out var hit))
+            {
+                pos.y = hit.point.y;
+
+                float deltaHeight = heightAffect * (pos.y - minY) / range;
+                if (Random.value > deltaHeight)
+                {
+                    clumpsArray[index++] = new GrassClump(pos);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         shader.SetFloat(timeID, Time.time);
         shader.Dispatch(kernelLeanGrass, groupSize, 1, 1);
