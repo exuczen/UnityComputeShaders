c3365f7b5087231ef365d306f75bba217a45a777
Marcin Świtała <marcin.switala.wwa@gmail.com>
2024-04-03 16:35:49 +0200

    Refactored SmoothFollowTarget.

--- UnityComputeShaders - start/Assets/Scripts/Control Scripts/SmoothFollowTarget.cs
+++ UnityComputeShaders - start/Assets/Scripts/Control Scripts/SmoothFollowTarget.cs
@@ -5,36 +5,27 @@ public class SmoothFollowTarget : MonoBehaviour
 {
     public GameObject target;
     public float[] limitsX;
-    Vector3 offset;
-    
 
-    bool b;
+    private Vector3 offset;
 
-    private void LateUpdate()
+    private void Start()
     {
         if (target == null)
         {
             target = GameObject.FindGameObjectWithTag("Player");
-            return;
         }
-        else
-        {
-            if (!b)
-            {
-                offset = transform.position - target.transform.position;
-                b = true;
-            }
+        offset = transform.position - target.transform.position;
+    }
 
-            Vector3 pos = target.transform.position + offset;
-            if (limitsX != null && limitsX.Length == 2)
-            {
-                pos.x = Mathf.Clamp(pos.x, limitsX[0], limitsX[1]);
-                //Debug.Log("pos.x clamped to " + pos.x);
-            }
-            transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 5);
-            transform.LookAt(target.transform);
-            return;
+    private void LateUpdate()
+    {
+        Vector3 pos = target.transform.position + offset;
+        if (limitsX != null && limitsX.Length == 2)
+        {
+            pos.x = Mathf.Clamp(pos.x, limitsX[0], limitsX[1]);
         }
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 5);
+        transform.LookAt(target.transform);
+        return;
     }
 }
-
